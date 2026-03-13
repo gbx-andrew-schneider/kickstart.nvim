b94faca0160580166bbfe8e4316f6d22f750d5f6
Andrew Schneider <andrew.schneider@gearbox.com>
2026-03-13 11:03:19 -0500

    add yaml lsp

--- init.lua
+++ init.lua
@@ -688,6 +688,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         'bash-language-server',
         'json-lsp',
+        'yaml-language-server',
         -- You can add other tools here that you want Mason to install
       })
 
@@ -700,6 +701,7 @@ require('lazy').setup({
         ['jsonls'] = {
           filetypes = { 'json', 'jsonc' },
         },
+        ['yamlls'] = {},
       })
 
       for name, server in pairs(servers) do
